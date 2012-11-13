75ebe85256e0b264bf4fa5774a29e18e7d8f8fd1
Jeff Douglas <jeff@jeffdouglas.com>
2012-11-13 07:02:25 -0500

    refactored call

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,7 @@ class Category < Salesforce
 
   def self.all(access_token) 
     set_header_token(access_token)   
-    request_url  = ENV['SFDC_APEXREST_URL'] + "/categories?fields=name,color__c&orderby=display_order__c&search=true"
-    Forcifier::JsonMassager.deforce_json(get(request_url))
+    get_apex_rest("/categories?fields=name,color__c&orderby=display_order__c&search=true")
   end
 
 end
