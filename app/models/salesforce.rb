fd39a3deb5523e80eeabcebf7e14e24f33189ef1
Jeff Douglas <jeff@jeffdouglas.com>
2012-12-04 05:19:52 -0500

    rest api calls for get, post and delete

--- app/models/salesforce.rb
+++ app/models/salesforce.rb
@@ -16,7 +16,11 @@ class Salesforce
   end  
 
   def self.soql_query(soql)
-    get(ENV['SFDC_REST_API_URL']+"/query?q=#{esc soql}")
+    begin
+      get(ENV['SFDC_REST_API_URL']+"/query?q=#{esc soql}")
+    rescue Exception => e
+      nil
+    end     
   end
 
   #
@@ -31,5 +35,50 @@ class Salesforce
   def self.get_apex_rest(url_string)
     Forcifier::JsonMassager.deforce_json(get(ENV['SFDC_APEXREST_URL']+"#{url_string}"))
   end
+
+  #
+  # Makes generic 'post' apex rest calls to create records in salesforce
+  # * *Args*    :
+  #   - sobject -> the sObject to create
+  #   - params -> hash with the fields and values to be used to create
+  # * *Returns* :
+  #   - a hash containing the following keys: success, message
+  # * *Raises* :
+  #   - ++ ->
+  #  
+  def self.post_apex_rest(sobject, params)
+    options = {
+      :body => params.to_json
+    }
+
+    begin
+      post_results = Forcifier::JsonMassager.deforce_json(post(ENV['SFDC_REST_API_URL']+
+        "/sobjects/#{sobject}", options))
+      {:success => true, :message => post_results['id']}
+    rescue Exception => e
+      {:success => false, :message => post_results}
+    end     
+  end  
+
+  #
+  # Makes generic 'delete' apex rest calls to delete a records in salesforce
+  # * *Args*    :
+  #   - sobject -> the sObject to create
+  #   - id -> the id of the record to delete
+  # * *Returns* :
+    #   - a hash containing the following keys: success, message
+  # * *Raises* :
+  #   - ++ ->
+  #  
+  def self.delete_apex_rest(sobject, id)
+    delete_results = Forcifier::JsonMassager.deforce_json(delete(ENV['SFDC_REST_API_URL']+
+      "/sobjects/#{sobject}/#{id}"))
+    # a return value of null means a successful delete
+    if delete_results
+      {:success => false, :message => delete_results.first['message']}
+    else
+      {:success => true, :message => 'Record successfully deleted.'}
+    end
+  end    
 
 end
