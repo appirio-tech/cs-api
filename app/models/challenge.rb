cd776f27779aa52d8fb83d046212e5473b2217d2
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-05 19:51:10 -0500

    added participating member

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -77,7 +77,7 @@ class Challenge < Salesforce
 
   def self.all(access_token, open, technology, platform, category, order_by, limit=25, offset=0) 
     params = {:open => open, :orderby => order_by, :limit => limit, :offset => offset,
-      :fields => 'Id,Challenge_Id__c,Name,Description__c,Total_Prize_Money__c,Challenge_Type__c,Days_till_Close__c,Registered_Members__c,Start_Date__c,End_Date__c,Is_Open__c,Community__r.Name'}
+      :fields => 'Id,Challenge_Id__c,Name,Description__c,Total_Prize_Money__c,Challenge_Type__c,Days_till_Close__c,Registered_Members__c,Participating_Members__c,Start_Date__c,End_Date__c,Is_Open__c,Community__r.Name'}
     params.merge!(:technology => technology) if technology
     params.merge!(:platform => platform) if platform
     params.merge!(:category => category) if category
