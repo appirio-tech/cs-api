f72d5f2481e2b773b36398bb65dc59da2b1816a8
Jeff Douglas <jeff@jeffdouglas.com>
2013-05-28 08:58:40 -0400

    removed airbrake

--- config/initializers/airbrake.rb
+++
@@ -1,5 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = 'da14214e8ee95a9c1e7b7d9bfe6d5516'
-  config.host = 'collect.airbrake.io'
-  config.logger = Logger.new("#{Rails.root}/log/airbrake.log")
-end
