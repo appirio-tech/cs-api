fd9ff524e8bc3eb80e65ddc6ae343514e3774d6b
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-05 12:34:15 -0400

    Fixed #1

--- app/models/judging.rb
+++ app/models/judging.rb
@@ -4,46 +4,10 @@ require 'challenge'
 class Judging  < Salesforce
 
   def self.save_scorecard_for_participant(access_token, participant_id, answers, comments, options)
-
-    begin
-
-      # mark the scorecard as rejected
-      if options[:delete_scorecard] == 'true'
-        update_in_salesforce(access_token, 'Challenge_Participant__c', {'id' => participant_id, 
-          'Has_Submission__c' => false, 'Status__c' => 'Submission Rejected'})
-        results_hash = {:success => true, :message => 'Scorecard marked as invalid and submission rejected.'}	
-
-      # update the answer for each question
-      else
-
-        answers.each do |key, value| 
-          results = update_in_salesforce(access_token, 'QwikScore_Question_Answer__c', 
-            {'id' => key, 'answer_text__c' => value, 'comments__c' => comments[key]})
-        end
-        results_hash = {:success => true, :message => 'Scorecard has been saved successfully.'}	
-
-        if options[:scored] == 'true'
-          # get the id of the scorecard for this member and judge
-          scorecard = query_salesforce(access_token, "select id from QwikScore_Scorecard__c 
-            where Challenge_Participant__c = '#{participant_id}' and 
-            Reviewer__r.name = '#{options[:judge_membername]}' limit 1")
-
-          set_header_token(access_token) 
-          grade_results = get_apex_rest("/gradeScorecard?scorecardId=#{scorecard.first.id}")	
-          if grade_results['success'].downcase == 'true'
-            results_hash = {:success => true, :message => 'Scorecard successfully submitted.'}	
-          else
-            results_hash = {:success => false, :message => 'There was an error submitting the scorecard.'}	
-          end
-        end
-
-      end
-
-    rescue Exception => e	
-      results_hash = {:success => false, :message => e.message}
-    end
-    results_hash
-
+    set_header_token(access_token) 
+    results = put_apex_rest("/scorecard", {:answers => answers.to_param, 
+      :comments => comments.to_param, :options => options.to_param, :participant_id => participant_id})   
+    {:success => results['success'].downcase.to_bool, :message => results['message']}
   end
 
   def self.find_scorecard_by_participant(access_token, participant_id, judge_membername)
